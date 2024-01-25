be39fbcde598764ca6aedf39cb04abd032a9bad2
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2024-01-25 08:49:00 -0400

    Agregar archivos de proyecto.

---
+++ HotelListingAPI/Data/HotelListListingDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListListingDbContext : DbContext
+    {
+        public HotelListListingDbContext(DbContextOptions options) : base(options) {
+        
+        }
+            public DbSet<Hotel> Hotels { get; set; }
+            public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name= "Republica Dominicana",
+                    ShortName="RD"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                }  
+            );
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   Id = 1,
+                   Name = "El Gran Hotel",
+                   Address ="Santo Domingo",
+                   ContryId=1,
+                   Rating=4.3
+               },
+               new Hotel
+               {
+                   Id = 2,
+                   Name = "El Jamaiquino",
+                   Address = "Negril",
+                   ContryId = 2,
+                   Rating = 4.2
+               }
+           );
+        }
+    }
+}
---
+++ HotelListingAPI/Migrations/20240124130146_SeededCountriesAndHotels.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListingAPI.Migrations
+{
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Republica Dominicana", "RD" },
+                    { 2, "Jamaica", "JM" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "ContryId", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Santo Domingo", 1, null, "El Gran Hotel", 4.2999999999999998 },
+                    { 2, "Negril", 2, null, "El Jamaiquino", 4.2000000000000002 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
